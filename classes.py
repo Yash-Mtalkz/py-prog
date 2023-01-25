c08a0cf4571d8c77a7ca25af752cda18049f8e08
Yash <syash3771@gmail.com>
2023-01-25 14:40:23 +0530

    classes with functions

--- classes.py
+++ classes.py
@@ -12,7 +12,20 @@ class Person:
         self.age = age
 
 
-p1 = Person("John", 36)
+p1 = Person("Yash", 22)
 
 print(p1.name)
 print(p1.age)
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+
+p1 = Person("Yash", 22)
+p1.myfunc()
